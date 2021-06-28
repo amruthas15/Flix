9b83bfd879b6b9da0db644588cdad8cf91f3eebf
Amrutha Srikanth <60552165+amruthas15@users.noreply.github.com>
2021-06-28 11:20:22 -0700

    AutoLayout updates

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -26,6 +26,7 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
     
     if([[NSUserDefaults standardUserDefaults] boolForKey:@"darkSwitch"])
     {
